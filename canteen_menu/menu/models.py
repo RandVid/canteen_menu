057fec52af7c5a358db0a0bb7408927ed181d03e
RandVid <mcflymartin221@gmail.com>
2023-12-14 20:08:19 +0200

    added is vegan badge

--- canteen_menu/menu/models.py
+++ canteen_menu/menu/models.py
@@ -4,9 +4,15 @@ from django.db import models
 # Create your models here.
 class Meal(models.Model):
     name = models.CharField(max_length=255)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     price = models.FloatField()
     in_menu = models.BooleanField()
+    is_vegan = models.BooleanField()
+    calories = models.IntegerField(null=True, blank=True)
+    proteins = models.IntegerField(null=True, blank=True)
+    fats = models.IntegerField(null=True, blank=True)
+    carbohydrates = models.IntegerField(null=True, blank=True)
+    mass = models.IntegerField(null=True, blank=True)
 
     def __str__(self):
         return f"{self.name}"
